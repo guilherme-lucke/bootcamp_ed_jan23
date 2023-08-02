22985168fc6d425ee80d9783c192e1d86c675617
Guilherme Lucke <luckegg@outlook.com>
2023-08-02 11:48:09 -0300

    Módulo 04

---
+++ MOD04/codigo_refatorado/main.py
@@ -0,0 +1,23 @@
+import datetime
+import time
+
+from schedule import repeat, every, run_pending
+from ingestors import DaySummaryIngestor #, TradesIngestor
+from writers import DataWriter
+
+if __name__ == "__main__":
+    day_summary_ingestor = DaySummaryIngestor(
+        writer=DataWriter,
+        coins=["BTC", "ETH", "LTC", "BCH"],
+        default_start_date=datetime.date(2021, 6, 1)
+    )
+
+#   trades_ingestor = TradesIngestor()
+
+@repeat(every(1).seconds)
+def job():
+    day_summary_ingestor.ingest()
+
+while True:
+    run_pending()
+    time.sleep(0.5)
